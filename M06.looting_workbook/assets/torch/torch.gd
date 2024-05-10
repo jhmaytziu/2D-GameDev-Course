b3b65da48ca0e5cc5681653a87ed20a534cda391
jhmaytziu <161763920+jhmaytziu@users.noreply.github.com>
2024-05-10 12:31:55 -0700

    M6 L9 P1-P2-P3

--- M06.looting_workbook/assets/torch/torch.gd
+++ M06.looting_workbook/assets/torch/torch.gd
@@ -1,7 +1,19 @@
-extends Node2D
+extends Area2D
 
 @onready var flame: Sprite2D = $Flame
 
 func _ready() -> void:
 	# This parameter of the shader material gives each flame a slightly different look and randomized animation.
 	flame.material.set("shader_parameter/offset", global_position * 0.1)
+
+func toogle_flame_visibility() -> void:
+	flame.visible = not flame.visible
+
+func _input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.pressed and
+		event.button_index == MOUSE_BUTTON_LEFT
+	)
+	if event_is_mouse_click:
+		toogle_flame_visibility()
